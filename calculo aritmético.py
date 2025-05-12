2d3eae6e75f26c95a270f8d423a709058e5e0f2b
Junior-Franco <juufrancoo15@gmail.com>
2025-05-11 21:21:06 -0300

    Delete calculo aritmético.py

--- calculo aritmético.py
+++
@@ -1,7 +0,0 @@
-print('Calculo aritmético\n')
-num=0
-quant=int(input('Quantos numeros deseja calcular a média? '))
-for i in range(quant):
-  num+=float(input(f'Digite o {i+1}° número: '))
-media=num/quant
-print(f'A média aritmética dos numeros é: {media}')
