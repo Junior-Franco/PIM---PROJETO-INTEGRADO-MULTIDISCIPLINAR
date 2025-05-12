c1cecbdb8c3d8d81e85ebb1c433d39f9c745a6ac
Junior-Franco <juufrancoo15@gmail.com>
2025-05-11 21:22:00 -0300

    Delete exercicio.py

--- exercicio.py
+++
@@ -1,7 +0,0 @@
-lugares = ["SUIÇA" , "CANADA" , "INGLATERRA" , "ESCOCIA" , "HOLANDA"]
-print(lugares)
-print(sorted(lugares))
-
-lugares = ["SUIÇA" , "CANADA" , "INGLATERRA" , "ESCOCIA" , "HOLANDA"]
-print(sorted(lugares))
-
